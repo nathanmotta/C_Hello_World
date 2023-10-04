356cb5da0be5a899fc304e6b1249814539e17a55
nathanmotta <142407439+nathanmotta@users.noreply.github.com>
2023-10-04 10:56:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace PrimeiroProjeto
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
